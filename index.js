3694c8e9743b0b5dad3c27c4afc66949e13b0ea7
Lorna Jane Mitchell <lorna@lornajane.net>
2020-06-16 14:20:15 +0100

    Fix line endings

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config({path: __dirname + '/.env'})
-​
+
 const NEXMO_API_KEY = process.env.NEXMO_API_KEY
 const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
 const TO_NUMBER = process.env.NEXMO_TO_NUMBER
 const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER
-​
+
 const Nexmo = require('nexmo')
-​
+
 const nexmo = new Nexmo({
   apiKey: NEXMO_API_KEY,
   apiSecret: NEXMO_API_SECRET
@@ -14,7 +14,7 @@ const nexmo = new Nexmo({
 const from = NEXMO_FROM_NUMBER
 const to = TO_NUMBER
 const text = 'A text message sent using the Nexmo SMS API'
-​
+
 nexmo.message.sendSms(447451288842, 447547113295, text, (err, responseData) => {
     if (err) {
         console.log(err);
