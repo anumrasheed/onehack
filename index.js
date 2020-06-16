d426fc3f17b5b75bc7622d60b801248211f32374
asankuappan <asankuappan@vonage.com>
2020-06-16 11:02:24 -0400

    commit index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const NEXMO_API_KEY = process.env.NEXMO_API_KEY
 const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
 const TO_NUMBER = process.env.NEXMO_TO_NUMBER
 const NEXMO_FROM_NUMBER = process.env.NEXMO_FROM_NUMBER
+const app = require('express')()
+const bodyParser = require('body-parser')
 
 const Nexmo = require('nexmo')
 
@@ -13,16 +15,36 @@ const nexmo = new Nexmo({
 })
 const from = NEXMO_FROM_NUMBER
 const to = TO_NUMBER
-const text = 'A text message sent using the Nexmo SMS API'
+const text = 'I need blood plasma for a patient. Can you please help? If you want to respond to this chat please login on www.donors.com'
 
-nexmo.message.sendSms(447451288842, 447547113295, text, (err, responseData) => {
-    if (err) {
-        console.log(err);
-    } else {
-        if(responseData.messages[0]['status'] === "0") {
-            console.log("Message sent successfully.");
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app
+  .route('/onehack')
+  .get(handleInboundSms)
+  .post(handleInboundSms)
+
+  function handleInboundSms(request, response) {
+   /* const params = Object.assign(request.query, request.body)
+    console.log(params)
+    response.status(204).send()*/
+//447460301777  447547113295
+    nexmo.message.sendSms(447451288842, 447460301777, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
         } else {
-            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            if(responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            }
         }
-    }
-})
+      
+    })
+    response.send('OK');
+  }
+
+  app.listen(process.env.PORT || 3000)
+
+
